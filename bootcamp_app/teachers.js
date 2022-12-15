2d197f4428f403e3e3847572ec51e4fb34081e80
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-12-14 20:10:26 -0500

    protect against SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,8 @@ const pool = new Pool({
 });
 
 const process = require('process');
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 pool.query(`
 SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
@@ -15,8 +17,8 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {console.log(res.rows)})
 .catch(err => console.error('query error', err.stack));
